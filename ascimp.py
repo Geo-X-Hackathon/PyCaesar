06ce175ed6816a1146a870b1065d3ba1fd0d7f69
Sebastian <s.walter@fu-berlin.de>
2018-03-13 14:12:58 +0100

    first working asci2numpy routine

--- ascimp.py
+++ ascimp.py
@@ -1,5 +1,5 @@
 from osgeo import gdal
-import os
+import numpy as np
 
 f = open('files.txt', 'r')
 
@@ -12,10 +12,25 @@ for line in f:
 f.close()
 sortedlist=sorted(numlist)
 
-for num in sortedlist:
-	filename='../CAESAR_data/D4/elev.dat'+str(num)+'.txt'
-	dataset=gdal.Open(filename, gdal.GA_ReadOnly)	
-	print("Size is {} x {} x {}".format(dataset.RasterXSize, dataset.RasterYSize, dataset.RasterCount))
-	dataset = None
+first=True
+
+#Get First Image and check dimensions
+filename='../CAESAR_data/D4/elev.dat'+str(sortedlist[0])+'.txt'
+dataset=gdal.Open(filename, gdal.GA_ReadOnly)
+print("Size of one DTM is {} x {} x {}".format(dataset.RasterXSize, dataset.RasterYSize, dataset.RasterCount))
+print("There are {} time steps".format(len(sortedlist)))
+#Build n-dimensional array (currently 3)
+array=np.empty([dataset.RasterYSize,dataset.RasterXSize,len(sortedlist)],dtype=np.float)
+print("Array size is {}".format(array.shape))
+
+#Cycle through all images and append to array
+for i in range(0,len(sortedlist)):
+	num=sortedlist[i]
+	name='../CAESAR_data/D4/elev.dat'+str(num)+'.txt'
+	ds=gdal.Open(name, gdal.GA_ReadOnly)	
+	a=ds.GetRasterBand(1).ReadAsArray()
+	array[:,:,i]=a
+	array[:,:,i][a<-999]=np.nan
+	ds = None
 
 
