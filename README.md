475ec9ff4a495267ee5824fccd1237bb290fa633
Sebastian <s.walter@fu-berlin.de>
2018-03-13 15:08:50 +0100

    single DTM case working!

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PyCaesar
+
+For PyQtGraph with OpebGL, (on debian/ubuntu), run:
+apt-get install python-qt4-gl
+
--- ascimp.py
+++ ascimp.py
@@ -1,5 +1,9 @@
 from osgeo import gdal
 import numpy as np
+from pyqtgraph.Qt import QtCore, QtGui
+import pyqtgraph as pg
+import pyqtgraph.opengl as gl
+
 
 f = open('files.txt', 'r')
 
@@ -20,11 +24,13 @@ dataset=gdal.Open(filename, gdal.GA_ReadOnly)
 print("Size of one DTM is {} x {} x {}".format(dataset.RasterXSize, dataset.RasterYSize, dataset.RasterCount))
 print("There are {} time steps".format(len(sortedlist)))
 #Build n-dimensional array (currently 3)
-array=np.empty([dataset.RasterYSize,dataset.RasterXSize,len(sortedlist)],dtype=np.float)
+#array=np.empty([dataset.RasterYSize,dataset.RasterXSize,len(sortedlist)],dtype=np.float)
+array=np.empty([dataset.RasterYSize,dataset.RasterXSize,1],dtype=np.float)
 print("Array size is {}".format(array.shape))
 
 #Cycle through all images and append to array
-for i in range(0,len(sortedlist)):
+#for i in range(0,len(sortedlist)):
+for i in range(0,1):
 	num=sortedlist[i]
 	name='../CAESAR_data/D4/elev.dat'+str(num)+'.txt'
 	ds=gdal.Open(name, gdal.GA_ReadOnly)	
@@ -33,4 +39,33 @@ for i in range(0,len(sortedlist)):
 	array[:,:,i][a<-999]=np.nan
 	ds = None
 
+min=np.nanmin(array)
+max=np.nanmax(array)
+normarray=array*3./(max-min)
+min=np.nanmin(normarray)
+max=np.nanmax(normarray)
+print min,max
 
+## Create a GL View widget to display data
+app = QtGui.QApplication([])
+w = gl.GLViewWidget()
+w.show()
+w.setWindowTitle('PyCAESAR: GLSurfacePlot')
+w.setCameraPosition(distance=50)
+
+## Add a grid to the view
+#g = gl.GLGridItem()
+#g.scale(2,2,1)
+#g.setDepthValue(10)  # draw grid after surfaces since they may be translucent
+#w.addItem(g)
+
+## Simple surface plot example
+## x, y values are not specified, so assumed to be 0:50
+z = np.squeeze(normarray[:,:,0])
+p1 = gl.GLSurfacePlotItem(z=z, shader='heightColor', computeNormals=False)
+p1.shader()['colorMap'] = np.array([0.2, 2, 0.5, 0.2, 1, 1, 0.2, 0, 2])
+p1.scale(1./50., 1./50., 1.)
+#p1.translate(-18, 2, 0)
+w.addItem(p1)
+
+QtGui.QApplication.instance().exec_()
